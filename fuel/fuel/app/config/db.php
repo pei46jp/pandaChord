daefa6255f715c7fa16ef170b7a93306f7694538
pei46n <mu@pei46.jp>
2024-04-23 15:41:28 +0900

    db init

--- fuel/fuel/app/config/db.php
+++ fuel/fuel/app/config/db.php
@@ -23,6 +23,9 @@
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+Log::error('pgsql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME']);
+Log::error($_ENV['DB_PW']);
+
 return array(
 
     'default' => array(
